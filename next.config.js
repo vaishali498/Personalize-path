0d3153cf05fdc89adc248de957da46fd554dc0af
Vaishali Maurya <113284951+vaishali498@users.noreply.github.com>
2024-06-03 22:12:44 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+require('dotenv').config();
+
+module.exports = {
+  reactStrictMode: true,
+  // other Next.js config options
+};
